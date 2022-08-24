f4de9947db2b7c83af33425b9da93cb2beab910e
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-08-24 14:50:28 +0200

    exercise 03 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let result = '';
+    let splitedStr = string.split('');
+    
+    let counter = splitedStr.length-1;
+    while(counter >= 0){
+        result += splitedStr[counter]
+        counter--;
+    }  
 
+    //return result
+    return result;
 };
 
 // Do not edit below this line
