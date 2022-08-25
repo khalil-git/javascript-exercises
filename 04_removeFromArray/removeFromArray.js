2953baa2449f1c9533eca23829b126717c0adb5c
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-08-25 14:01:38 +0200

    exercise 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    
+    // filter items not in args array
+    let result =  arr.filter(item => {
+        if (!args.includes(item)) {
+          return item;
+        }
+      })
+    
+    // return result
+    return result;
 };
 
 // Do not edit below this line
