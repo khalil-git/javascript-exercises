32a62ab5f7077277ba94748c27777400b73df76e
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-08-29 13:25:11 +0200

    exercise 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {  
+  // °C = [(°F-32)×5]/9
+  let celsius = ((fahrenheit -32) * 5) / 9;  
+  celsius = parseFloat(celsius.toFixed(1));
 
+  //return temp rounded to one decimal place
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  // °F=(°C × 9/5) + 32
+  let fahrenheit = (celsius * 1.8) + 32;   
+  fahrenheit = parseFloat(fahrenheit.toFixed(1));
+  //return temp rounded to one decimal place
+  return fahrenheit;
 };
 
 // Do not edit below this line
