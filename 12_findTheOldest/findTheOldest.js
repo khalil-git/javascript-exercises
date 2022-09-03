660dab54a699ea2be7a1f35e927e97f1080ea0f5
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-09-03 14:26:39 +0200

    exercise 012 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  
+  //use Date to gey full year  
+  let today = new Date();
+  let year = today.getFullYear();
+  let age;
+  let maxAgePerson;
 
+  maxAgePerson = people.reduce((max, person) => {
+    // add age property to object 
+    if (person.yearOfDeath) {        
+      // death person  
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      // still alive person   
+      person.age = year - person.yearOfBirth;
+    }
+    // return max age person
+    return person.age > max.age ? person : max;
+  }, people[0]);
+
+  return maxAgePerson;
 };
 
 // Do not edit below this line
