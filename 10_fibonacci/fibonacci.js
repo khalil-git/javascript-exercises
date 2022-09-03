d9f4d7555b8c8e876a2aae06209ca695def8c1cb
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-09-03 14:21:08 +0200

    exercise 010 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    n = parseInt(n);
 
+    if(n < 0)
+        return "OOPS";    
+        
+    if(n <= 1)
+        return n;
+
+ return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
